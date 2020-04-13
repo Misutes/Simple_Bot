44598efd944d61040fa1de9585e9e087b8392ade
Misutes <misutes@list.ru>
2020-04-13 14:09:27 +0300

    Simple functional

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
-import vk_api
+import vk_api, keyboard, Text
+from keyboard import new_keyboard
+
+
 
 token = '79eee7ae818b1db0d38b95f8911b1576e3d3a325c3622f6feaead4f3de1b09cdb0285cc9ac8308fb471f2'
 vk_session = vk_api.VkApi(token=token)
@@ -7,19 +10,16 @@ vk_session = vk_api.VkApi(token=token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
-list_of_greeting = ['привет', 'здравствуйте', 'добрый утро', 'добрый день', 'добрый вечер',
-                    'привет.', 'здравствуйте.', 'добрый утро.', 'добрый день.', 'добрый вечер.',
-                    'привет!', 'здравствуйте!', 'добрый утро!', 'добрый день!', 'добрый вечер!']
-
-hello_message = ', привет! Я официальный бот-помощник библиотеки им. К.А.Тимирязева. \n' \
-                'И вот что я умею: \n' \
-                '1. Могу помочь Вам продлить книгу \n'
-
-missunderstanding_message = 'Я из другой эпохи, эти вопросы не ко мне!'
+user_list = []
 
 
-def message(message):
-    vk_session.method('messages.send', {'user_id': event.user_id, 'message': message, "random_id": 0})
+def message(message, keyboard):
+    vk_session.method('messages.send', {
+        'user_id': event.user_id,
+        'message': message,
+        "random_id": 0,
+        "keyboard": keyboard
+    })
 
 
 while True:
@@ -28,9 +28,17 @@ while True:
             user_message = event.text
             user_message = user_message.lower()
             user_info = session_api.users.get(user_ids=event.user_id)
-            if user_message in list_of_greeting:
-                message(user_info[0]['first_name'] + hello_message)
-            elif user_message == '1':
-                message('вот ссылочка: \n https://vk.com/app6013442_-43349586#form_id=1')
-            else:
-                pass
+            user_info = user_info[0]
+            if user_message in Text.list_of_greeting:
+                if user_info['id'] not in user_list:
+                    user_list.append(user_info['id'])
+                    message(user_info['first_name'] + Text.hello_message, new_keyboard(keyboard.function_keyboard))
+                else:
+                    message(user_info['first_name'] + str(', категорически приветствую! Напомниаю, что я могу: '),
+                            new_keyboard(keyboard.function_keyboard))
+            elif user_message.lower() == 'продление книги':
+                message(user_info['first_name'] + str(' , Вы можете сделать это здесь'),
+                        new_keyboard(keyboard.link_keyboard))
+            elif user_message.lower() == 'написать кое-что':
+                message(user_info['first_name'] + str(', я же умненький?'),
+                        None)
---
+++ keyboard.py
@@ -0,0 +1,46 @@
+import json
+
+
+def get_text_buttons(label, color, payload=""):
+    return {
+        "action": {
+            "type": "text",
+            "payload": json.dumps(payload),
+            "label": label
+        },
+        "color": color
+    }
+
+
+def get_link_buttons(label, link):
+    return {
+        "action": {
+            "type": "open_link",
+            "label": label,
+            "link": link
+        }
+    }
+
+
+def new_keyboard(keyboard):
+    keyboard = str(json.dumps(keyboard, ensure_ascii=False))
+    return keyboard
+
+
+function_keyboard ={
+    "inline": True,
+    "buttons": [
+            [get_text_buttons(label='Продление книги', color='positive')],
+            [get_text_buttons(label='Написать кое-что', color='positive')]
+    ]
+}
+
+link_keyboard = {
+        "inline": True,
+        "buttons": [
+            [
+                get_link_buttons('Продление книги',
+                                 'https://vk.com/app6013442_-43349586?form_id=1#form_id=1')
+            ]
+        ]
+    }
